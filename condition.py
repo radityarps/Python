a2a7d17df2aac2b4921347c3101c4133f4a14ded
RadityaRPS <136070858+radityarps@users.noreply.github.com>
2023-10-03 19:48:05 +0700

    Delete condition.py

--- condition.py
+++
@@ -1,8 +0,0 @@
-# STRUKTUR KONDISI
-# cek_umur.py
-umur = input("Berapa umur kamu: ")
-umur1 = input("Berapa umur maksimal boleh ambil SIM: ")
-if umur >= umur1:
- print("Kamu boleh membuat SIM")
-else:
- print("Kamu belum boleh membuat SIM")
