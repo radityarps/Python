827ca3332d66373d462d93ba5063d8131b9df843
radityarps <radityarafifps@gmail.com>
2023-10-17 10:15:38 +0700

    adding comment for how the code work

--- Klasifikasi lanjut S3.py
+++ Klasifikasi lanjut S3.py
@@ -1,11 +1,12 @@
-nipDosen = input("Masukkan NIP anda: ")
+nipDosen = input("Masukkan NIP anda: ")  # Input untuk memasukkan NIP
+# Mengiris variabel nipDosen dari 0 - 3, untuk mendapatkan tahun lahir dari inputan tadi
 tahunLahir = int(nipDosen[0:4])
 tahunSekarang = 2023
-umur = tahunSekarang - tahunLahir
+umur = tahunSekarang - tahunLahir  # Operasi untuk mengetahui umur user
 
-if umur >= 60:
+if umur >= 60:  # Jika umur lebih dari/sama dengan 60, tidak bisa S3
     print("Tidak bisa melanjutkan S3")
-elif umur > 40 and umur < 60:
+elif umur > 40 and umur < 60:  # Jika umur lebih dari 40 dan kurang dari 60, s3 biaya sendiri
     print("Lanjut S3 menggunakan biaya sendiri")
-elif umur <= 40:
+elif umur <= 40:  # Jika umur kurang dari/sama dengan 40, S3 Beasiswa
     print("Lanjut S3 menggunakan beasiswa")
