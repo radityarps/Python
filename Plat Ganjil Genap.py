ebf354283162fda98bf658c4c839628de0192909
radityarps <radityarafifps@gmail.com>
2023-10-17 09:39:48 +0700

    fix

--- Plat Ganjil Genap.py
+++ Plat Ganjil Genap.py
@@ -4,16 +4,16 @@ tanggalSekarang = input("Masukkan tanggal sekarang denga format 14-10-2023: ")
 tanggal = int(tanggalSekarang[0:2])
 
 
-def checkGanjil():
+def checkGanjilGenap():
     if (nomorPlat % 2 == 0 and tanggal % 2 == 0) or (nomorPlat % 2 != 0 and tanggal % 2 != 0):
         return True
     else:
         return False
 
 
-checkGanjil()
+checkGanjilGenap()
 
-if checkGanjil == True:
+if checkGanjilGenap == True:
     print("Boleh Lewat")
 else:
     print("Tidak Boleh Lewat")
